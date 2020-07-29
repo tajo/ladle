9871b957688418536336246f8bebf5c828afb3be
Vojtech Miksu <vojtech@uber.com>
2020-07-28 18:03:36 -0700

    make react peer dep

--- src/a1.stories.tsx
+++ src/a1.stories.tsx
@@ -4,7 +4,7 @@ export const Zebra: React.FC = () => {
   const [val, setVal] = React.useState(true);
   return (
     <div>
-      <h1>Zebra</h1>
+      <h1>AAA</h1>
       <button onClick={() => setVal(false)}>
         button {val ? "true" : "false"}
       </button>
@@ -16,8 +16,8 @@ export const Hahaasdasd: React.FC = () => {
   const [val, setVal] = React.useState("");
   return (
     <div>
-      <h1>This is a new asd asdas d asd</h1>
-      And it stays here so I can just keep typing :)
+      <h1>This is a new asd asdas asdasdd asd</h1>
+      And it stays here so I can ajust keep typing :)
       <input onChange={(e) => setVal(e.target.value)} value={val} />
     </div>
   );
