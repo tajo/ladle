b486b68e4b0f1874672d0e28b600c7f157861d9e
Vojtech Miksu <vojtech@uber.com>
2021-01-18 19:52:37 -0700

    Add CLI flags

--- lib/cli/types.ts
+++ lib/cli/types.ts
@@ -14,19 +14,41 @@ export type GeneratedStoriesT = {
 };
 
 export type ThemeT = "light" | "dark" | "auto";
+type OutputT = "dashboard" | "stream";
 
 export type DevParamsT = {
   stories: string;
   port: number;
   theme: ThemeT;
+  output: OutputT;
+  open: string;
 };
 
 export type BuildParamsT = {
   stories: string;
-  outDir: string;
+  out: string;
   theme: ThemeT;
+  sourcemap: boolean;
+  baseUrl: string;
+  optimize: boolean;
 };
 
 export type PluginOptionsT = {
   storyGlob: string;
+};
+
+export type ConfigT = {
+  stories: string;
+  theme: ThemeT;
+  serve: {
+    open: string;
+    port: number;
+    output: OutputT;
+  };
+  build: {
+    out: string;
+    sourcemap: boolean;
+    baseUrl: string;
+    optimize: boolean;
+  };
 };
