50a9b91a34876e04a6486c575600927103bf1c61
Vojtech Miksu <vojtech@uber.com>
2021-01-29 14:47:36 -0700

    Improve styles

--- lib/app/src/icons.tsx
+++ lib/app/src/icons.tsx
@@ -46,7 +46,7 @@ export const Bulb: React.FC<{}> = () => {
 
 export const Page: React.FC<{}> = () => {
   return (
-    <div style={{ width: "10px", marginRight: "0.5em" }}>
+    <div style={{ width: "10px", marginRight: "0.5em", flexShrink: 0 }}>
       <svg fill="currentColor" viewBox="0 0 768 1024">
         <path d="M509 64l195 218v669q0 3-4 6t-9 3H77q-5 0-9-3t-4-6V73q0-3 4-6t9-3h432zm29-64H77Q45 0 22.5 21.5T0 73v878q0 30 22.5 51.5T77 1024h614q32 0 54.5-21.5T768 951V257zm-26 256V0h-64v256q0 26 19 45t45 19h253v-64H512z" />
       </svg>
