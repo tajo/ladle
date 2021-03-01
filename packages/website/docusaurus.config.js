d1026ba8e42732d778ee2859bc0479210c2c6ace
Vojtech Miksu <vojtech@uber.com>
2021-02-28 19:26:28 -0800

    Introduce yarn workspaces

---
+++ packages/website/docusaurus.config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  title: "Ladle",
+  tagline: "Develop and test your React components faster.",
+  url: "https://www.ladle.dev",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.svg",
+  organizationName: "tajo", // Usually your GitHub org/user name.
+  projectName: "ladle", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "Ladle",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
+        },
+        { to: "blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/tajo/ladle",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "light",
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} Ladle`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/tajo/ladle/edit/master/packages/website/",
+        },
+        blog: {
+          showReadingTime: false,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/tajo/ladle/edit/master/packages/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
