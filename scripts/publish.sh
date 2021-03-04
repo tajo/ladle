aea3ba74725de89716cb416274cbb3240af9c293
Vojtech Miksu <vojtech@uber.com>
2021-03-04 13:06:57 -0800

    ci: test

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,19 +2,12 @@
 
 set -eo pipefail
 
-echo "WHAT"
-echo "$INFO"
-echo "--late--"
-
 # Setup SSH keys so we can push lerna commits and tags to master branch
 
-mkdir -p /root/.ssh
-ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts
-echo "$GIT_DEPLOY_KEY" > /root/.ssh/id_rsa
-chmod 400 /root/.ssh/id_rsa
-
-# The script is run as root so we need to allow npm to execute scripts as root.
-echo "unsafe-perm = true" >> ~/.npmrc
+mkdir -p ~/.ssh
+ssh-keyscan -t rsa github.com > ~/.ssh/known_hosts
+echo "$GIT_DEPLOY_KEY" > ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
 
 # Setup git and pull tags for Lerna
 
