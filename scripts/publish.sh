6691bee0aa1562cd991303da999afcd30888ea1e
Vojtech Miksu <vojtech@uber.com>
2021-03-04 15:06:14 -0800

    fix: change

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -20,4 +20,4 @@ git rebase origin/master
 git fetch --unshallow --tags
 
 # Run lerna
-node_modules/.bin/lerna publish --yes --registry https://registry.npmjs.org
+node_modules/.bin/lerna publish --yes
