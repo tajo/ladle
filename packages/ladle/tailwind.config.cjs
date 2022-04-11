9be5172361de2580c22c306e6a4e3ad58bf7cc1e
Vinicius Pacheco <vin175pacheco@gmail.com>
2022-04-11 19:13:51 -0300

    feat: build styles with postcss and tailwind

---
+++ packages/ladle/tailwind.config.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ["./lib/app/src/styles/*.css"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "var(--ladle-primary-50)",
+          100: "var(--ladle-primary-100)",
+          200: "var(--ladle-primary-200)",
+          300: "var(--ladle-primary-300)",
+          400: "var(--ladle-primary-400)",
+          500: "var(--ladle-primary-500)",
+          600: "var(--ladle-primary-600)",
+          700: "var(--ladle-primary-700)",
+          800: "var(--ladle-primary-800)",
+          900: "var(--ladle-primary-900)",
+        },
+        accent: {
+          50: "var(--ladle-accent-50)",
+          100: "var(--ladle-accent-100)",
+          200: "var(--ladle-accent-200)",
+          300: "var(--ladle-accent-300)",
+          400: "var(--ladle-accent-400)",
+          500: "var(--ladle-accent-500)",
+          600: "var(--ladle-accent-600)",
+          700: "var(--ladle-accent-700)",
+          800: "var(--ladle-accent-800)",
+          900: "var(--ladle-accent-900)",
+        },
+      },
+    },
+  },
+};
