71d6d79ac2af78e8124aa9706755ada381e59410
Vojtech Miksu <vojtech@uber.com>
2022-05-03 10:44:50 -0700

    Remove macos from CI, seems slow/unreliable

--- release.sh
+++ release.sh
@@ -2,11 +2,15 @@
 
 set -eo pipefail
 
-echo "//registry.npmjs.org/:_authToken=$NODE_AUTH_TOKEN" > ~/.npmrc
+echo "Running release"
+echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
 
+echo "Build @ladle/react"
 turbo run build --filter=@ladle/react
+
+echo "Changeset publish"
 changeset publish
 
-# build and publish @ladle/react-cjs
+echo "Build and publish @ladle/react-cjs"
 cd packages/ladle/cjs
 pnpm publish
