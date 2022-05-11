3d0e66a6bd9ed6b8958ab64fa5e63b1303ebcf84
tajo <vojtech@miksu.cz>
2022-05-11 01:04:36 -0700

    Update docs - describe parsing errors

--- packages/ladle/lib/app/src/no-stories-error.tsx
+++ packages/ladle/lib/app/src/no-stories-error.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import * as React from "react";
-import { Code, Link } from "./ui";
+import { Link } from "./ui";
 
 const NoStoriesError: React.FC<{ error: string }> = ({ error }) => {
   console.log(error);
@@ -9,33 +9,12 @@ const NoStoriesError: React.FC<{ error: string }> = ({ error }) => {
       <h1>❌ Error when discovering stories ❌</h1>
       <pre>{error}</pre>
       <p>
-        <strong>Please restart Ladle, after fixing this issue.</strong>
+        <Link href="https://ladle.dev/docs/stories#limitations">
+          More information.
+        </Link>
       </p>
-      <h2>General limitations</h2>
       <p>
-        There are <b>some limitations</b> when it comes to the syntax of
-        stories:
-      </p>
-      <ul>
-        <li>
-          <Code>storyName</Code> and the default <Code>title</Code> need to be a
-          string literals. <Code>Story.storyName = "Renamed"</Code> is fine but{" "}
-          <Code>Story.storyName = "Renamed" + "Oof"</Code> not.
-        </li>
-        <li>
-          File names and named exports (or storyNames) are normalized and used
-          together as an unique identifier for each story. There cannot be two
-          stories with the same ID. Please, rename your stories or story files.
-        </li>
-        <li>
-          <Code>meta</Code> parameter needs to be serializable so it can be
-          shared through the{" "}
-          <Link href="https://ladle.dev/docs/meta">meta.json</Link> file.
-        </li>
-      </ul>
-      <p>
-        These limitations are in place to support some features like the
-        automatic code-splitting and meta file.
+        <strong>Please restart Ladle after fixing this issue.</strong>
       </p>
       <p>
         <Link href="https://github.com/tajo/ladle">Github</Link>
