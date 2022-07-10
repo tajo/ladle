d6c6a0d46cb215e6c8f7c38dd48bd942e4ee6781
Philippe Deslauriers <philde@chainguard.dev>
2022-07-10 10:46:33 -0700

    Fix resolve.alias compat issue.

--- e2e/config/ladle-vite.config.js
+++ e2e/config/ladle-vite.config.js
@@ -2,4 +2,8 @@ export default {
   server: {
     open: "none",
   },
+  resolve: {
+    // See #184 - this tests the package for the resolve.alias RegExp format.
+    alias: [{ find: /any-cool-regex/, replacement: "any-string" }],
+  },
 };
