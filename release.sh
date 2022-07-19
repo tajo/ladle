67ade32c5e0b667ca71cd92f2936a273ab186740
tajo <vojtech@miksu.cz>
2022-07-18 20:35:30 -0700

    Fix tests and remove workflow cjs publish

--- release.sh
+++ release.sh
@@ -9,17 +9,3 @@ turbo run build --filter=@ladle/react
 
 echo "Changeset publish"
 changeset publish
-
-echo "Build and publish @ladle/react-cjs"
-cd packages/ladle/cjs
-
-remote_version=$(pnpm view @ladle/react-cjs --json | jq -r '.version')
-local_version=$(jq -r '.version' package.json)
-
-if [ "$local_version" != "$remote_version" ]; then
-  echo "Publishing @ladle/react-cjs@$local_version"
-  pnpm publish
-else
-  echo "@ladle/react-cjs@$local_version has been published already."
-fi
-
