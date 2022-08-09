ca20f830a38ed5af197f932c53b6a73258d0a527
tajo <vojtech@miksu.cz>
2022-08-09 00:28:09 -0700

    Update snaps

--- e2e/playwright/src/abc.stories.tsx
+++ e2e/playwright/src/abc.stories.tsx
@@ -1,5 +1,10 @@
 export const First = () => {
-  return <h1>First</h1>;
+  // not rendering a text since fonts render differently in different operation systems
+  // and we use this package in our Github Actions CI which runs Ubuntu and Windows
+  // so doing a blue rectangle instead to keep the setup simple
+  // ideally, you should use something like Docker to have a consistent setup across
+  // the local and remote environments
+  return <div style={{ width: 200, height: 100, backgroundColor: "blue" }} />;
 };
 
 export const Second = () => {
