89db9045905f12c1751bd8946edc76107b945d8a
tajo <vojtech@miksu.cz>
2022-08-30 13:34:52 -0700

    Fix HMR and add e2e tests.

---
+++ e2e/provider/tests/add.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from "@playwright/test";
+import fs from "fs";
+
+const code = `export const Button = () => {
+  return <button id="new-button">New</button>;
+};
+`;
+
+test.afterEach(async () => {
+  if (fs.existsSync("./src/add.stories.tsx")) {
+    fs.unlinkSync("./src/add.stories.tsx");
+  }
+});
+
+test.only("adding new story triggers a full page reload", async ({ page }) => {
+  await page.goto("http://localhost:61106/?story=hmr--with-state");
+  fs.writeFileSync("./src/add.stories.tsx", code);
+  await page.waitForTimeout(1000);
+  await expect(page.locator("nav")).toHaveText("AddHelloHmrWith state");
+  await page.goto("http://localhost:61106/?story=add--button");
+  await expect(page.locator("#new-button")).toHaveText("New");
+});
---
+++ e2e/provider/tests/hmr.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from "@playwright/test";
+import fs from "fs";
+
+const before = `export const WithState = () => {
+  return (
+    <>
+      <input id="state-input" />
+      <button>Button</button>
+    </>
+  );
+};
+`;
+
+const after = `export const WithState = () => {
+  return (
+    <>
+      <input id="state-input" />
+      <button>Button</button>
+      <button id="new-button">New</button>
+    </>
+  );
+};
+`;
+
+test.beforeEach(async () => {
+  fs.writeFileSync("./src/hmr.stories.tsx", before);
+});
+
+test.afterEach(async () => {
+  fs.writeFileSync("./src/hmr.stories.tsx", before);
+});
+
+test("hmr with fast refresh works", async ({ page }) => {
+  await page.goto("http://localhost:61106/?story=hmr--with-state");
+  await page.locator("#state-input").fill("some state");
+  fs.writeFileSync("./src/hmr.stories.tsx", after);
+  await page.waitForSelector("#new-button", { timeout: 3000 });
+  await expect(page.locator("#state-input")).toHaveValue("some state");
+  await expect(page.locator("#new-button")).toHaveText("New");
+});
