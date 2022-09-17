ca14b40082f65cc81a5139749167b20e500a1753
tajo <vojtech@miksu.cz>
2022-09-17 01:16:00 -0700

    Fix next publish script

--- packages/ladle/publish-next.js
+++ packages/ladle/publish-next.js
@@ -10,15 +10,18 @@ console.log(`Publishing @ladle/react ${version}`);
 
 const pkgJson = JSON.parse(fs.readFileSync("./package.json"));
 const oldVersion = pkgJson.version;
+const oldContextVersion = pkgJson.dependencies["@ladle/react-context"];
 pkgJson.version = version;
+pkgJson.dependencies["@ladle/react-context"] = oldContextVersion.split(":")[1];
 fs.writeFileSync("./package.json", JSON.stringify(pkgJson, null, 2));
 
 try {
-  execSync("npm publish");
+  execSync("npm publish --tag next");
 } catch (e) {
   console.log(e);
   console.log("Publish failed, reverting package.json");
 }
 
 pkgJson.version = oldVersion;
+pkgJson.dependencies["@ladle/react-context"] = oldContextVersion;
 fs.writeFileSync("./package.json", JSON.stringify(pkgJson, null, 2));
