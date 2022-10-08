4ef2e1d23ab27b77f65922388e248da686061f9c
Anthony Frehner <frehner@users.noreply.github.com>
2022-10-07 21:51:06 -0600

    Add scripts that modify the package.json before publishing (#275)
    In order to get TS types working correctly for "Node16" and "NodeNext"

--- packages/ladle/publish-next.js
+++ packages/ladle/publish-next.js
@@ -2,6 +2,10 @@
 
 import { execSync } from "child_process";
 import fs from "fs";
+import {
+  preparePackageJsonForPublish,
+  revertPackageJson,
+} from "./scripts/package-types-helpers.js";
 
 const shortHash = execSync("git rev-parse --short HEAD").toString().trim();
 const version = `0.0.0-next-${shortHash}`;
@@ -13,6 +17,7 @@ const oldVersion = pkgJson.version;
 const oldContextVersion = pkgJson.dependencies["@ladle/react-context"];
 pkgJson.version = version;
 pkgJson.dependencies["@ladle/react-context"] = oldContextVersion.split(":")[1];
+preparePackageJsonForPublish(pkgJson);
 fs.writeFileSync("./package.json", JSON.stringify(pkgJson, null, 2));
 
 try {
@@ -24,4 +29,5 @@ try {
 
 pkgJson.version = oldVersion;
 pkgJson.dependencies["@ladle/react-context"] = oldContextVersion;
+revertPackageJson(pkgJson);
 fs.writeFileSync("./package.json", JSON.stringify(pkgJson, null, 2));
--- release.sh
+++ release.sh
@@ -7,5 +7,14 @@ echo "Running release"
 echo "Build @ladle/react"
 turbo run build --filter=@ladle/react
 
+echo "Update package.json"
+node ./packages/ladle/scripts/update-package-types.js
+
+echo "Generate Types":
+pnpm --filter @ladle/react types
+
 echo "Changeset publish"
 changeset publish
+
+echo "Revert package.json"
+node ./packages/ladle/scripts/revert-package-types.js
