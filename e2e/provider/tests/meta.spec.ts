c3a1b985770ea96cacb79cb402e2ab5e82acc7bf
Mats Gunnerød <mats.gunnerod@zaptec.com>
2022-12-15 22:06:32 +0100

    Removed test.only typo

--- e2e/provider/tests/meta.spec.ts
+++ e2e/provider/tests/meta.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
-test.only("Story meta is available in provider", async ({ page }) => {
+test("Story meta is available in provider", async ({ page }) => {
   await page.goto("http://localhost:61106/?story=meta--story-meta");
   await expect(page.locator("#myMeta")).toHaveText("This is my meta");
 });
