7969d81e2e8fc41907ed9d0a08f7cdfe6f712982
Mel Massadian <mel@melmassadian>
2023-01-11 15:15:25 +0100

    fix: 🧪 test differing locally and on the CI

--- e2e/provider/tests/add.spec.ts
+++ e2e/provider/tests/add.spec.ts
@@ -16,7 +16,7 @@ test.only("adding new story triggers a full page reload", async ({ page }) => {
   await page.goto("http://localhost:61106/?story=hmr--with-state");
   fs.writeFileSync("./src/add.stories.tsx", code);
   await page.waitForTimeout(1000);
-  await expect(page.locator("nav")).toHaveText("HelloHmrWith stateMeta");
+  await expect(page.locator("nav")).toHaveText("AddHelloHmrWith stateMeta");
   await page.goto("http://localhost:61106/?story=add--button");
   await expect(page.locator("#new-button")).toHaveText("New");
 });
