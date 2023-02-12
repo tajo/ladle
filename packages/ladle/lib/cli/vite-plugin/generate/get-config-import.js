71673281a908bd6348db66bbeccce42f47d7a7c9
tajo <vojtech@miksu.cz>
2023-02-11 22:13:09 -0700

    Pass config.mjs values into the client through serialization and virtual module

--- packages/ladle/lib/cli/vite-plugin/generate/get-config-import.js
+++ packages/ladle/lib/cli/vite-plugin/generate/get-config-import.js
@@ -1,18 +1,38 @@
 import fs from "fs";
 import path from "path";
-import cleanupWindowsPath from "./cleanup-windows-path.js";
 
 /**
  * @param {string} configFolder
  */
-const getConfigImport = (configFolder) => {
+const getConfigImport = async (configFolder) => {
   const configPath = path.join(configFolder, "config.mjs");
   const configExists = fs.existsSync(configPath);
   let configCode = `export let config = {};\n`;
   if (configExists) {
-    configCode += `import customConfig from '${cleanupWindowsPath(
-      path.join(configFolder, "config.mjs"),
-    )}';\nconfig = customConfig;\n`;
+    const config = (await import(configPath)).default;
+    let serializedConfig = {};
+    try {
+      serializedConfig = JSON.stringify({
+        ...(config.stories ? { stories: config.stories } : {}),
+        ...(config.addons ? { addons: config.addons } : {}),
+        ...(config.defaultStory ? { defaultStory: config.defaultStory } : {}),
+        ...(config.base ? { base: config.base } : {}),
+        ...(config.mode ? { mode: config.mode } : {}),
+        ...(config.storyOrder
+          ? {
+              storyOrder: Array.isArray(config.storyOrder)
+                ? config.storyOrder
+                : config.storyOrder.toString(),
+            }
+          : {}),
+      });
+    } catch (e) {
+      console.log(
+        `config.stories, config.addons, config.defaultStory, config.base, config.mode and config.storyOrder need to be serializable.`,
+      );
+      process.exit(1);
+    }
+    configCode += `\nconfig = ${serializedConfig};\n`;
   }
   return `${configCode}`;
 };
