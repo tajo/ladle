010f257bc5639264d67696d2597293ca98fcc0b6
tajo <vojtech@miksu.cz>
2023-02-15 11:43:36 -0700

    Fix useLadleContext()

---
+++ e2e/addons/src/context.tsx
@@ -0,0 +1,8 @@
+import { useLadleContext } from "@ladle/react";
+
+function Context() {
+  const { globalState } = useLadleContext();
+  return <div id="context-div">{JSON.stringify(globalState)}</div>;
+}
+
+export default Context;
