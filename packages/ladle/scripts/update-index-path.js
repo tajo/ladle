ffdc373545b34860ad1ee7dfba1891403c2880e5
tajo <vojtech@miksu.cz>
2023-02-14 23:12:12 -0700

    Publish

--- packages/ladle/scripts/update-index-path.js
+++ packages/ladle/scripts/update-index-path.js
@@ -1,6 +1,17 @@
-import fs from "fs";
+#!/usr/bin/env node
 
-const indexPath = "./packages/ladle/typings-for-build/app/index.html";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const indexPath = path.join(__dirname, "../typings-for-build/app/index.html");
 const index = fs.readFileSync(indexPath, "utf8");
 
-fs.writeFileSync(indexPath, index.replace("index.tsx", "index.js"));
+fs.writeFileSync(
+  indexPath,
+  index
+    .replace("index.tsx", "index.js")
+    .replace("init-side-effects.ts", "init-side-effects.js"),
+);
