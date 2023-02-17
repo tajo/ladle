d599e2c802f4ef36dc6cdff835cb5876e5104d16
tajo <vojtech@miksu.cz>
2023-02-16 17:50:30 -0700

    Fix unit tests

--- e2e/config-ts/tests/add.spec.ts
+++ e2e/config-ts/tests/add.spec.ts
@@ -7,16 +7,16 @@ const code = `export const Button = () => {
 `;
 
 test.afterEach(async () => {
-  if (fs.existsSync("./src/add.stories.tsx")) {
-    fs.unlinkSync("./src/add.stories.tsx");
+  if (fs.existsSync("./src/add.show.tsx")) {
+    fs.unlinkSync("./src/add.show.tsx");
   }
 });
 
 test("adding new story triggers a full page reload", async ({ page }) => {
-  await page.goto("http://localhost:61106/?story=hmr--with-state");
-  fs.writeFileSync("./src/add.stories.tsx", code);
+  await page.goto("http://localhost:61108");
+  fs.writeFileSync("./src/add.show.tsx", code);
   await page.waitForTimeout(1000);
-  await expect(page.locator("nav")).toHaveText("AddHelloHmrWith stateMeta");
-  await page.goto("http://localhost:61106/?story=add--button");
+  await expect(page.locator("nav")).toHaveText("AddHelloWorld");
+  await page.goto("http://localhost:61108/?story=add--button");
   await expect(page.locator("#new-button")).toHaveText("New");
 });
