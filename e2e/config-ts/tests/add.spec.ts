9b67ab275b8e544a2b5a9d00c8b9e09bf4dd7b42
tajo <vojtech@miksu.cz>
2023-02-16 17:58:48 -0700

    Fix e2e

--- e2e/config-ts/tests/add.spec.ts
+++ e2e/config-ts/tests/add.spec.ts
@@ -16,7 +16,7 @@ test("adding new story triggers a full page reload", async ({ page }) => {
   await page.goto("http://localhost:61108");
   fs.writeFileSync("./src/add.show.tsx", code);
   await page.waitForTimeout(1000);
-  await expect(page.locator("nav")).toHaveText("AddHelloWorld");
+  await expect(page.locator("nav")).toContainText("Add");
   await page.goto("http://localhost:61108/?story=add--button");
   await expect(page.locator("#new-button")).toHaveText("New");
 });
