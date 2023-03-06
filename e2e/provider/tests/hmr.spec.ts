5139536e424afb4797fdb8565f852521e8defdda
tajo <vojtech@miksu.cz>
2023-03-06 15:32:59 -0700

    Skip hmr test in prod mode

--- e2e/provider/tests/hmr.spec.ts
+++ e2e/provider/tests/hmr.spec.ts
@@ -30,11 +30,13 @@ test.afterEach(async () => {
   fs.writeFileSync("./src/hmr.stories.tsx", before);
 });
 
-test("hmr with fast refresh works", async ({ page }) => {
-  await page.goto("http://localhost:61106/?story=hmr--with-state");
-  await page.locator("#state-input").fill("some state");
-  fs.writeFileSync("./src/hmr.stories.tsx", after);
-  await page.waitForSelector("#new-button", { timeout: 5000 });
-  await expect(page.locator("#state-input")).toHaveValue("some state");
-  await expect(page.locator("#new-button")).toHaveText("New");
-});
+if (!process.env.SKIP_HMR) {
+  test("hmr with fast refresh works", async ({ page }) => {
+    await page.goto("http://localhost:61106/?story=hmr--with-state");
+    await page.locator("#state-input").fill("some state");
+    fs.writeFileSync("./src/hmr.stories.tsx", after);
+    await page.waitForSelector("#new-button", { timeout: 5000 });
+    await expect(page.locator("#state-input")).toHaveValue("some state");
+    await expect(page.locator("#new-button")).toHaveText("New");
+  });
+}
