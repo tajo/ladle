efa6e894af343cf582b88a47db762c7c98d59266
tajo <vojtech@miksu.cz>
2023-03-06 15:21:01 -0700

    test

--- e2e/provider/tests/hmr.spec.ts
+++ e2e/provider/tests/hmr.spec.ts
@@ -34,7 +34,7 @@ test("hmr with fast refresh works", async ({ page }) => {
   await page.goto("http://localhost:61106/?story=hmr--with-state");
   await page.locator("#state-input").fill("some state");
   fs.writeFileSync("./src/hmr.stories.tsx", after);
-  await page.waitForSelector("#new-button", { timeout: 3000 });
+  await page.waitForSelector("#new-button", { timeout: 5000 });
   await expect(page.locator("#state-input")).toHaveValue("some state");
   await expect(page.locator("#new-button")).toHaveText("New");
 });
