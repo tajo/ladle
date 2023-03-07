87469ba44597e52f7c3d9b27577b87ab0480debf
tajo <vojtech@miksu.cz>
2023-03-07 12:43:54 -0700

    Fix e2e hosts

--- e2e/programmatic/build.js
+++ e2e/programmatic/build.js
@@ -2,5 +2,6 @@ import build from "@ladle/react/build";
 
 build({
   port: 61105,
+  host: "127.0.0.1",
   storyOrder: ["hello--world", "hello--ayo"],
 });
--- e2e/programmatic/preview.js
+++ e2e/programmatic/preview.js
@@ -2,4 +2,5 @@ import preview from "@ladle/react/preview";
 
 preview({
   previewPort: 61105,
+  host: "127.0.0.1",
 });
