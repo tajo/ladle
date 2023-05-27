efd76b1df4eb0da7ca4462f3661a2aec4d54c7f8
tajo <vojtech@miksu.cz>
2023-05-26 22:27:15 -0700

    Include all stories into optimizeDeps.entries

--- packages/ladle/lib/cli/vite-base.js
+++ packages/ladle/lib/cli/vite-base.js
@@ -1,6 +1,7 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
+import { globby } from "globby";
 import { fileURLToPath } from "url";
 import tsconfigPaths from "vite-tsconfig-paths";
 import getAppRoot from "./get-app-root.js";
@@ -89,6 +90,14 @@ const getBaseViteConfig = async (ladleConfig, configFolder, viteConfig) => {
   );
   debug("Executed from the ladle monorepo: %s", inladleMonorepo);
 
+  const storyEntries = (
+    await globby(
+      Array.isArray(ladleConfig.stories)
+        ? ladleConfig.stories
+        : [ladleConfig.stories],
+    )
+  ).map((story) => path.join(process.cwd(), story));
+
   /**
    * @type {import('vite').InlineConfig}
    */
@@ -139,6 +148,7 @@ const getBaseViteConfig = async (ladleConfig, configFolder, viteConfig) => {
         path.join(process.cwd(), ".ladle/components.jsx"),
         path.join(process.cwd(), ".ladle/components.tsx"),
         path.join(process.cwd(), ".ladle/components.ts"),
+        ...storyEntries,
       ],
     },
     plugins: [
