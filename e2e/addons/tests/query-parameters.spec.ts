a3aaff233d71cb768f5afbc5b109bfd01a2308ef
dyoshikawa <yoshikawa.daio@classmethod.jp>
2023-09-26 10:38:36 +0900

    add click the menu and open the story case to e2e tests

--- e2e/addons/tests/query-parameters.spec.ts
+++ e2e/addons/tests/query-parameters.spec.ts
@@ -1,7 +1,21 @@
 import { test, expect } from "@playwright/test";
 
-test.only("remain non-ladle query parameters", async ({ page }) => {
+test("when directly open the story, remain non-ladle query parameters", async ({
+  page,
+}) => {
   await page.goto("/?story=query-parameters--query-parameters");
+  await page.waitForSelector("[data-storyloaded]");
+  const url = page.url();
+  expect(url).toContain("story=query-parameters--query-parameters");
+  expect(url).toContain("foo=bar");
+});
+
+test("when click the menu and open the story, remain non-ladle query parameters", async ({
+  page,
+}) => {
+  await page.goto("/?story=a11y--issues"); // Open a some story
+  await page.getByText("Query parameters").click(); // Open the accordion of query parameters stories
+  await page.getByRole("link", { name: "Query parameters" }).click(); // Open the target story
   await page.waitForSelector("[data-storyloaded]");
   const url = page.url();
   expect(url).toContain("story=query-parameters--query-parameters");
