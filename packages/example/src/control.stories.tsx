101424fb49ff726b6d9994ccf1137408b67babde
tajo <vojtech@miksu.cz>
2023-10-02 18:33:16 -0700

    Typo

--- packages/example/src/control.stories.tsx
+++ packages/example/src/control.stories.tsx
@@ -1,16 +1,10 @@
 import type { Story } from "@ladle/react";
 
-export default {
-  title: "ThisIsSoLongThatItShouldBeTruncatedOrElse/Haha",
-};
-
 const Card: Story<{
   label: string;
 }> = ({ label }) => <p>Label: {label}</p>;
 
 export const CardHello = Card.bind({});
-
-export const ThisIsExtremelyLongAndShouldBeTruncated = () => <hr></hr>;
 
 CardHello.args = {
   label: "Hello",
